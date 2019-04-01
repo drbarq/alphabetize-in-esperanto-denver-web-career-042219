2fa52e808851745fdacba22c593dfae02e09fdb5
Joe Tustin <j.tustin@gmail.com>
2019-04-01 21:37:46 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 def alphabetize(arr)
 
-esperanto_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+esperanto_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
 
   arr.sort_by do |sentence|
     sentence.chars.collect do |letter|
