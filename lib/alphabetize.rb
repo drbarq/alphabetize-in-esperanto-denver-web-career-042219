bb7815e95d7e67ed71e469420217e5418f56f51b
Joe Tustin <j.tustin@gmail.com>
2019-04-01 21:29:22 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,9 +5,10 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
 def alphabetize(arr)
   arr.sort_by do |sentence|
     sentence.chars do |character|
+      ESPERANTO_ALPHABET(character)
     binding.pry
 
-  end 
+  end
   end
 
 end
