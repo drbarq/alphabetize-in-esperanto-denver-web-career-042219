6f676c6348816392432f83267ec9aaf7e4efc76c
Joe Tustin <j.tustin@gmail.com>
2019-04-01 21:32:02 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,7 +5,7 @@ require 'pry'
 def alphabetize(arr)
 
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
-  
+
   arr.sort_by do |sentence|
     sentence.chars.collect do |letter|
       ESPERANTO_ALPHABET.index(character)
