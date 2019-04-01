2b7463ef5d6eb89133379a96793f37bb40fd6ddb
Joe Tustin <j.tustin@gmail.com>
2019-04-01 21:33:44 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -8,10 +8,9 @@ esperanto_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
   arr.sort_by do |sentence|
     sentence.chars.collect do |letter|
-      
+
       esperanto_ALPHABET.index(letter)
       binding.pry
-
 
   end
   end
